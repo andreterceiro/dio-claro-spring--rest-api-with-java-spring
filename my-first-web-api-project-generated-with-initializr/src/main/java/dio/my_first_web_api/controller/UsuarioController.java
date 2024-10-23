1416e06a53683e0128ddfb03a716ec128e1483d7
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-23 15:59:11 -0300

    feat: creating a RequestMapping to group all the requests with the same prefix

--- my-first-web-api-project-generated-with-initializr/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
+++ my-first-web-api-project-generated-with-initializr/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -6,13 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.my_first_web_api.model.Usuario;
 import dio.my_first_web_api.repository.UsuarioRepository;
 
+
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
@@ -21,12 +24,12 @@ public class UsuarioController {
         return new String();
     }
     
-    @GetMapping("/usuarios")
+    @GetMapping()
     public List<Usuario> obterUsuarios() {
         return repository.findAll();
     }
 
-    @DeleteMapping("/usuarios/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id) {
         repository.deleteById(id);
     }
