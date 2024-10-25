4a7730df614286e05c1c090ef2d614688ce2e882
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-25 15:08:43 -0300

    feat: editing (PUT HTTP verb) an user

--- my-first-web-api-project-generated-with-initializr/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
+++ my-first-web-api-project-generated-with-initializr/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,5 +41,11 @@ public class UsuarioController {
     public void adicionarUsuario(@RequestBody Usuario usuario) {
         System.out.println(usuario);
         System.out.println("adicionando um usuário");
+    }
+
+    @PutMapping("/edit")
+    public void editarUsuario(@RequestBody Usuario usuario) {
+        System.out.println(usuario);
+        System.out.println("editando um usuário");
     }
 }
